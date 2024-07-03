2dba3e491374b6cdd76c726e572cbf3e73a03fe2
Igor Shutrov <igor@shutrov.ru>
2024-07-03 18:21:28 +0500

    use own nav bar colors

--- app/src/main/java/com/example/gridtestapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gridtestapp/ui/theme/Theme.kt
@@ -45,7 +45,9 @@ fun GridTestAppTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
+            window.navigationBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = false
         }
     }
 
